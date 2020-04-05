8706d8e98e408f7d3477021b82eccaee7326709b
UnuJee <apple8250@naver.com>
2020-04-05 20:28:03 +0900

    Update 10718.js

--- src/10718.js
+++ src/10718.js
@@ -1,2 +1,10 @@
-console.log("강한친구 대한육군");
-console.log("강한친구 대한육군");
+// console.log("강한친구 대한육군");
+// console.log("강한친구 대한육군");
+
+// console.log("강한친구 대한육군\n\
+// 강한친구 대한육군");
+
+// console.log(`강한친구 대한육군
+// 강한친구 대한육군`);
+
+console.log("강한친구 대한육군\n" + "강한친구 대한육군");
