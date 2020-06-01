af941f207abaf98a7b6dd17cf2f7b922013c09b9
UnuJee <42885087+UnuJee@users.noreply.github.com>
2020-06-01 15:58:07 +0900

    Update 1157.js

--- src/1157.js
+++ src/1157.js
@@ -1,1 +1,31 @@
-내일 하자~~
+const n = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+const STR = n.shift();
+const obj = {};
+
+for(const ele of STR){
+  if(Object.keys(obj).includes(ele.toUpperCase())){
+    obj[ele.toUpperCase()] += 1;
+  }
+  else{
+    obj[ele.toUpperCase()] = 1;
+  }
+}
+
+const values = Object.values(obj);
+const keys = Object.keys(obj);
+
+let num = 0;
+let key = "";
+
+for(let i=0; i<values.length; i++){
+  if(num < values[i]){
+    num = values[i];
+    key = keys[i];
+  }
+  else if(num === values[i]){
+    key = "?";
+  }
+
+}
+
+console.log(key);
