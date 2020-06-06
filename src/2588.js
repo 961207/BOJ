c4114c76f615bd018a09840c941753c8701a0d7c
UnuJee <42885087+UnuJee@users.noreply.github.com>
2020-06-06 14:18:04 +0900

    Update 2588.js

--- src/2588.js
+++ src/2588.js
@@ -1,1 +1,9 @@
-나중에
+const n = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+
+const NUM1 = n[0];
+const NUM2 = n[1];
+
+console.log(+NUM1 * +NUM2.charAt(2));
+console.log(+NUM1 * +NUM2.charAt(1));
+console.log(+NUM1 * +NUM2.charAt(0));
+console.log(+NUM1 * +NUM2);
