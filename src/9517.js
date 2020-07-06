f027253a6d5c3037b9759a97b4622bf3155e6812
Yunwoo Ji <42885087+unukr@users.noreply.github.com>
2020-07-07 00:21:07 +0900

    Create 9517.js

---
+++ src/9517.js
@@ -0,0 +1,20 @@
+const n=require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+
+const K = +n.shift();
+let i = K-1;
+const N = +n.shift();
+
+let tt = 0;
+
+for(let j=0; j<N; j++){
+  const time = +n[j].split(" ")[0];
+  const ans = n[j].split(" ")[1];
+  tt += time;
+  if(tt >= 210){
+    console.log(i+1);
+    break;
+  }
+  if(ans === "T"){
+    i = (i+1)%8;
+  }
+}
