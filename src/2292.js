53446257dd5cf0a3e43d6b0f39f085659681b576
Yunwoo Ji <42885087+Unuuuuu@users.noreply.github.com>
2020-07-19 16:13:02 +0900

    Update 2292.js

--- src/2292.js
+++ src/2292.js
@@ -1,14 +1,26 @@
+// const n=+require('fs').readFileSync('dev/stdin').toString().trim();
+
+// const dp = [];
+
+// let i=0;
+// while(true){
+//   if(i === 0) dp[i] = 1;
+//   else dp[i] = dp[i-1] + i*6;
+
+//   if(dp[i]>=n) break;
+//   i++;
+// }
+
+// console.log(dp.length);
+
 const n=+require('fs').readFileSync('dev/stdin').toString().trim();
 
-const dp = [];
-
 let i=0;
+let j=1;
 while(true){
-  if(i === 0) dp[i] = 1;
-  else dp[i] = dp[i-1] + i*6;
-
-  if(dp[i]>=n) break;
+  j += (6*i);
+  if(j>=n) break;
   i++;
 }
 
-console.log(dp.length);
+console.log(i+1);
