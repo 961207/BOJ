ad7b30386e5f98300e87b74f7896f611ad954224
Yunwoo Ji <42885087+Unuuuuu@users.noreply.github.com>
2020-08-06 14:00:43 +0900

    Update 11718.js

--- src/11718.js
+++ src/11718.js
@@ -1,4 +1,6 @@
-let n=require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-for(const ele of n){
-  console.log(ele);
-}
+// let n=require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+// for(const ele of n){
+//   console.log(ele);
+// }
+
+console.log(require('fs').readFileSync('dev/stdin')+"");
