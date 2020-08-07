a130aa82372fbf46f11a738f03edab8b26404228
Yunwoo Ji <42885087+Unuuuuu@users.noreply.github.com>
2020-08-07 12:18:35 +0900

    Update 4153.js

--- src/4153.js
+++ src/4153.js
@@ -1,9 +1,17 @@
+// let n=require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+// for(const ele of n){
+//   const[a,b,c]=ele.split(" ").map(v=>+v);
+//   if(a === 0) break;
+//   if(Math.pow(a,2)+Math.pow(b,2)===Math.pow(c,2)) console.log("right");
+//   else if(Math.pow(a,2)+Math.pow(c,2)===Math.pow(b,2)) console.log("right");
+//   else if(Math.pow(b,2)+Math.pow(c,2)===Math.pow(a,2)) console.log("right");
+//   else console.log("wrong");
+// }
+
 let n=require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
 for(const ele of n){
-  const[a,b,c]=ele.split(" ").map(v=>+v);
-  if(a === 0) break;
-  if(Math.pow(a,2)+Math.pow(b,2)===Math.pow(c,2)) console.log("right");
-  else if(Math.pow(a,2)+Math.pow(c,2)===Math.pow(b,2)) console.log("right");
-  else if(Math.pow(b,2)+Math.pow(c,2)===Math.pow(a,2)) console.log("right");
+  let[a,b,c]=ele.split(" ").map(v=>+v).sort((a,b)=>a-b);
+  if(a===0) break;
+  if(a**2+b**2===c**2) console.log("right");
   else console.log("wrong");
 }
