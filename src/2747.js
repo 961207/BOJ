ca6b6da69935a0352a8cf3fe04b09c3658cf5f88
Yunwoo Ji <42885087+Unuuuuu@users.noreply.github.com>
2020-08-12 13:34:31 +0900

    Update 2747.js

--- src/2747.js
+++ src/2747.js
@@ -1,8 +1,13 @@
-let n=+require('fs').readFileSync('dev/stdin').toString();
-const arr=new Array(n+1);
-for(const [i,v] of arr.entries()){
-  if(i===0) arr[i]=0;
-  else if(i===1) arr[i]=1;
-  else arr[i] = arr[i-1] + arr[i-2];
-}
-console.log(arr[arr.length-1]);
+// let n=+require('fs').readFileSync('dev/stdin').toString();
+// const arr=new Array(n+1);
+// for(const [i,v] of arr.entries()){
+//   if(i===0) arr[i]=0;
+//   else if(i===1) arr[i]=1;
+//   else arr[i] = arr[i-1] + arr[i-2];
+// }
+// console.log(arr[arr.length-1]);
+
+let n=+require('fs').readFileSync('dev/stdin').toString(),
+    arr=[0,1];
+for(let i=1; i<n; i++) arr.push(arr[i-1]+arr[i]);
+console.log(arr[n]);
