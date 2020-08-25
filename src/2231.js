c8ea629628dd2dff3ed49d78e70ae29939b75f1b
Yunwoo Ji <42885087+Unuuuuu@users.noreply.github.com>
2020-08-25 22:09:32 +0900

    Update 2231.js

--- src/2231.js
+++ src/2231.js
@@ -1,16 +1,28 @@
+// const n = parseInt(require('fs').readFileSync('dev/stdin').toString().trim());
+// for(let i=n-54; i<=n; i++){
+//   if(i === n){
+//     console.log(0);
+//     break;
+//   } else{
+//     let sum = i;
+//     for(const ele of i.toString().split("")){
+//       sum += +ele;
+//     }
+//     if(sum === n){
+//       console.log(i);
+//       break;
+//     }
+//   }
+// }
+
 const n = parseInt(require('fs').readFileSync('dev/stdin').toString().trim());
-for(let i=n-54; i<=n; i++){
-  if(i === n){
-    console.log(0);
-    break;
-  } else{
-    let sum = i;
-    for(const ele of i.toString().split("")){
-      sum += +ele;
-    }
+(function(){
+  for(let i=1; i<=n; i++){
+    let sum = i + i.toString().split("").reduce((a,c)=>parseInt(a)+parseInt(c),0);
     if(sum === n){
       console.log(i);
-      break;
+      return;
     }
   }
-}
+  console.log(0);
+})()
