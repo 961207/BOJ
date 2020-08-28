e2fb04575fa29afee27abf4889ae7bef0c8994ad
Yunwoo Ji <42885087+Unuuuuu@users.noreply.github.com>
2020-08-28 11:40:55 +0900

    Update 7568.js

--- src/7568.js
+++ src/7568.js
@@ -1,14 +1,18 @@
-const n = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
-const t = parseInt(n.shift());
-const results = new Array(t);
-for(let i=0; i<t; i++){
-	let sum = 1;
-	for(let j=0; j<t; j++){
-		if(i === j) continue;
-		const [w1,h1] = n[i].split(" ");
-		const [w2,h2] = n[j].split(" ");
-		if((w1 < w2) && (h1 < h2)) sum += 1;
-	}
-	results[i] = sum;
-}
-console.log(results.join(" "));
+// const n = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
+// const t = parseInt(n.shift());
+// const results = new Array(t);
+// for(let i=0; i<t; i++){
+// 	let sum = 1;
+// 	for(let j=0; j<t; j++){
+// 		if(i === j) continue;
+// 		const [w1,h1] = n[i].split(" ");
+// 		const [w2,h2] = n[j].split(" ");
+// 		if((w1 < w2) && (h1 < h2)) sum += 1;
+// 	}
+// 	results[i] = sum;
+// }
+// console.log(results.join(" "));
+
+const n = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').filter((v,i)=>i>0).map(v=>v.split(' ').map(Number));
+
+console.log(n.map(v1=>n.filter(v2=>(v1[0] < v2[0] && v1[1] < v2[1])).length + 1).join(' '));
