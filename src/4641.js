ef29bbbe5eb573403ceaf4968b3e3a7ff40cb5ef
Yunwoo Ji <apple8250@naver.com>
2020-09-04 13:53:06 +0900

    Update 4641.js

--- src/4641.js
+++ src/4641.js
@@ -1,11 +1,17 @@
+// const n = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
+// for(const ele of n){
+//     if(ele === "-1") break;
+//     const arr = ele.split(" ").map(Number);
+//     let sum = 0;
+//     for(const num of arr){
+//         if(num === 0) break;
+//         if(arr.includes(num*2)) sum++;
+//     }
+//     console.log(sum);
+// }
+
 const n = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
 for(const ele of n){
     if(ele === "-1") break;
-    const arr = ele.split(" ").map(Number);
-    let sum = 0;
-    for(const num of arr){
-        if(num === 0) break;
-        if(arr.includes(num*2)) sum++;
-    }
-    console.log(sum);
+    console.log(ele.split(" ").map(Number).filter((v,_,arr)=>v!==0 && arr.includes(v*2)).length)
 }
